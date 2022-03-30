cfc5c8a7b88af25a0af81e7769502daa3941cba5
Zheng <aheze@getfind.app>
2022-03-29 18:18:43 -0700

    Working decode words

--- list.js
+++ list.js
@@ -9,12 +9,20 @@ let description = params.description;
 let icon = params.icon;
 let color = params.color;
 let dateCreated = params.dateCreated;
-let words = params.words;
+
 
 console.log(title)
 console.log(description)
 console.log(icon)
 console.log(color)
 console.log(dateCreated)
+
+let search = window.location.search.split("&words=")
+let wordsString = search[1]
+let wordsEncoded = wordsString.split(",")
+let words = wordsEncoded.map(decodeString)
 console.log(words)
 
+function decodeString(string) {
+    return decodeURI(string)
+}
